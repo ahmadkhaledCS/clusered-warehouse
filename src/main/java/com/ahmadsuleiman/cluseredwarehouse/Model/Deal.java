0c0401aff2fb0c2b7c6d2555c5a7a6b53a4dddfa
ahmad khaled CS <77671100+ahmadkhaledCS@users.noreply.github.com>
2023-10-10 20:53:35 +0300

    Added proper data to the Deal object

--- src/main/java/com/ahmadsuleiman/cluseredwarehouse/Model/Deal.java
+++ src/main/java/com/ahmadsuleiman/cluseredwarehouse/Model/Deal.java
@@ -8,6 +8,9 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -15,6 +18,9 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collation = "deals")
 public class Deal {
     @Id
-    private int id;
-    private String name;
+    private String id;
+    private String fromCurrency;
+    private String toCurrency;
+    private Date timestamp;
+    private BigDecimal amount;
 }
